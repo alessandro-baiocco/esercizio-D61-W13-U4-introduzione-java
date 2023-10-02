2232866ef7afd273b7949149ae4cafbea0eac1fb
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-10-02 15:27:30 +0200

    esercizio 3a fatto

--- cose/src/Esercizio3.java
+++ cose/src/Esercizio3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //Esercizio #3
 //        Implementare i seguenti metodi
 //
@@ -9,7 +11,15 @@
 //        Scrivere una main che utilizzi in sequenza i tre metodi chiedendo l'input dei dati all'utente
 public class Esercizio3 {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("esercizio 3a : inserisci 2 valori" );
+        float val1Es3 = input.nextFloat();
+        float val2Es3 = input.nextFloat();
+        System.out.println(perimetroRettangolo(val1Es3 , val2Es3));
+    }
 
+    public static double perimetroRettangolo(double a , double b){
+        return a * b;
     }
 
 }
